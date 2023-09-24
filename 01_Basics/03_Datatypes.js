8945fe443c6c8a599f2b8e27b7549b7fa0db4ed2
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-09-24 11:50:58 +0000

    Datatypes in javascript

---
+++ 01_Basics/03_Datatypes.js
@@ -0,0 +1,13 @@
+const StudentName="Hitesh"
+let age=20
+let Progress=true
+let StudentID="asia123@gmail.com"
+
+console.log(StudentName)
+let name="saba"//string
+let RollNumber=24 //integer
+let value=null;
+
+console.log(typeof "name");
+console.log(typeof(RollNumber));
+console.log(typeof(null));

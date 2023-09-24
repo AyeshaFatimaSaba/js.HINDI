87f3c2c19e10983954f3a3a4efc0d4b9e1390724
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-09-24 16:27:28 +0000

    Conversion of datatypes

---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="Ayesha"
+
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(valueInNumber);
+
+//"33"=>33
+//33abc=>NaN
+//true=1;false=0
+
+let isloggedIn="Ayesha"
+
+let booleanIsLoggedIn=Boolean(isloggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1=>true;0=>false
+//""=false
+//"Ayesha"=true
+
+let someNumber=33
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

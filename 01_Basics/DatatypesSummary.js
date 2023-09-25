5fe55613d08828f5705b1130f68ebe8aa6ad1090
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-09-25 19:45:39 +0000

    DatatypesSummary.js

--- 01_Basics/DatatypesSummary.js
+++ 01_Basics/DatatypesSummary.js
@@ -5,11 +5,28 @@
 
 //3 type:Arrays,Objects,Functions
 
-//const score=100
-//const scorevalue=100.3
-//const isLoggedIn=false
-//const outsideTemp=null
-//let userEmail;
+const score=100
+const scorevalue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
 const id=Symbol('123');
 const anotherId=Symbol('123');
-console.log(id===anotherId);
+//console.log(id===anotherId);
+
+const BigNumber=34556357665356754n
+
+const heroes=["Shaktiman","Krishh","Baalwir"]
+//letmyObject
+{
+    name:"Ayesha"
+    age:29
+}
+
+const myFunction=function()
+{
+  //  console.log("Hello World");
+}
+
+
+console.log(typeof BigNumber);

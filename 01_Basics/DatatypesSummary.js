17bb468b657893a648941e969ef7020430eed88b
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-09-25 17:06:36 +0000

    DatatypeSummary

---
+++ 01_Basics/DatatypesSummary.js
@@ -0,0 +1,15 @@
+//  Primitive
+//7 Datatypes:String,Numbers,Boolean,null,undefined,symbol,BigInt
+
+//Reference Datatypes(Non-Primitives)
+
+//3 type:Arrays,Objects,Functions
+
+//const score=100
+//const scorevalue=100.3
+//const isLoggedIn=false
+//const outsideTemp=null
+//let userEmail;
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id===anotherId);

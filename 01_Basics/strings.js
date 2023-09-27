e87492ae3a6c8d79d0c6d62e7fb912a6f3f28553
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-09-27 19:21:41 +0000

    Strings.js

---
+++ 01_Basics/strings.js
@@ -0,0 +1,35 @@
+const name ="Ayesha";
+const repoCount=50;
+//console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+//const gameName=new String('AyeshaFa');
+
+//console.log(gameName[0]);
+//console.log(gameName__proto__);
+
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+//console.log(gameName.toLowerCase());
+
+//console.log(gameName.charAt(3));
+
+//console.log(gameName.indexOf('s'));
+
+const gameName=new String('Ayesha-Fa');
+//const newString=gameName.substring(0,4);
+//console.log(newString);
+
+const anotherString=gameName.slice(-8,4);
+//console.log(anotherString);
+
+const newStringOne="  Ayesha  "
+//console.log(newStringOne.trim());
+
+const url="https://hitesh.com/hitesh%20choudhary"
+//console.log(url.replace('%20','-h'));
+
+//console.log(url.includes('ayesha'));
+
+//console.log(gameName.split('-'));
+

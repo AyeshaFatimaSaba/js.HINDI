65601cd008930fabeb31fd8e6367747ed5adfbdd
AyeshaFatimaSaba <145834674+AyeshaFatimaSaba@users.noreply.github.com>
2023-10-21 17:11:46 +0000

    Arrays.js

--- 01_Basics/Arrays.js
+++ 01_Basics/Arrays.js
@@ -1,6 +1,39 @@
 const myArr=[0,1,2,3,4,5]
 
 const Heroes=["Shaktiman","Superman","Batman"]
-console.log(myArr[0]);
+//console.log(myArr[0]);
 const myArr2=new Array(1,2,3,4)
-console.log(myArr[1]);
+//console.log(myArr[1]);
+
+//+++++++++++++++++++++Array Methods++++++++++++++++++++++++++
+
+myArr.push(6)
+//console.log(myArr);
+
+myArr.push(7)
+//console.log(myArr);
+
+myArr.pop();
+//console.log(myArr);
+
+myArr.unshift(12);
+//console.log(myArr);
+
+myArr.shift();
+//console.log(myArr);
+
+//console.log(myArr.includes(9));
+
+//console.log(myArr);
+
+//console.log(myArr.indexOf(19));
+
+const newArr=myArr.join()
+console.log(newArr);
+
+console.log(typeof newArr);
+
+
+
+
+
